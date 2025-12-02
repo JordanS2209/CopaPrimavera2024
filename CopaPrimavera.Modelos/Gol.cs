c332b6e15d9a99cf7ef6f84b6350f275caf81e4b
JordanS2209 <jssalcedoc49@gmail.com>
2025-12-02 13:03:01 -0500

    camibios

--- CopaPrimavera.Modelos/Gol.cs
+++ CopaPrimavera.Modelos/Gol.cs
@@ -10,13 +10,13 @@ namespace CopaPrimavera.Modelos
     public class Gol
     {
         [Key]
-        public Guid Id { get; set; }
+        public int Id { get; set; }
 
         [Required]
-        public Guid PartidoId { get; set; }
+        public int PartidoId { get; set; }
         public Partido Partido { get; set; } = null!;
 
-        public Guid? JugadorId { get; set; }
+        public int? JugadorId { get; set; }
         public Jugador? Jugador { get; set; }
 
         // Minuto del gol
